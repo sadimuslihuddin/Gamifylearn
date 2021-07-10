50d7815443bc809fe91379f8d0e956f6166d6017
sadimuslihuddin <74242805+sadimuslihuddin@users.noreply.github.com>
2021-07-10 07:54:47 +0700

    Delete JS directory

--- JS/materiGL.js
+++
@@ -1,8 +0,0 @@
-$(".chs").on('click', function(e){
-    if($(this).parent().parent().find("#cheatbar").hasClass("disappear")){
-    $(this).parent().parent().find("#cheatbar").removeClass("disappear");
-    }else{
-    $(this).parent().parent().find("#cheatbar").addClass("disappear");
-    };
-});
-
